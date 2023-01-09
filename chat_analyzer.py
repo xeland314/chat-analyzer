d0890015fd259ca139fc389012024b4dc8ed4278
Christopher Villamarín <alexpila.314@gmail.com>
2023-01-08 22:09:34 -0500

    Fix type of file in CLI
    str has been changed by Optional[str] to avoid errors.

--- chat_analyzer.py
+++ chat_analyzer.py
@@ -1,9 +1,10 @@
 from analyzer import WhatsappAnalyzer
 from os.path import exists
 from typer import run, Option, Argument
+from typing import Optional
 
 def main(
-    file: str = Argument(..., help="File name o path."), 
+    file: Optional[str] = Argument(None, help="File name o path."), 
     install: bool = Option(
         False, "--install", "-i", help="Install nltk dependencies."
     ),
@@ -14,10 +15,11 @@ def main(
         15, "--emojis", "-e", help="Number of emojis to show in the summary"
     )
 ):
-    if install:
+    if install and file is None:
         import nltk
         nltk.download("punkt")
         nltk.download("stopwords")
+        return
 
     if not exists(file):
         raise FileNotFoundError(f"El archivo {file} no existe.")
